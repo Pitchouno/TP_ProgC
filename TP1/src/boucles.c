f505674c4ad49b6f7929636690967093662eb905
Pitchouno <mathisphilippe60@gmail.com>
2025-10-24 11:42:21 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int i;
+  int j;
+  int taille;
+  printf("la taille du triangle: ");
+  scanf("%d", &taille);
+  
+  for(i=1;(i<=taille)&&(taille<=10);i++){
+    
+    for(j=1;j<=i;j++){
+      
+      
+            if (i == 1 || i == 5)
+                printf("* ");
+            else {
+                
+                if (j % 2 == 1)
+                    printf("* ");
+                else
+                    printf("# ");
+            }
+
+    }
+    printf("\n");
+  }
+
+
+}
